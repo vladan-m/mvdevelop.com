19434c40d8aadd1400a20e0bf8c4333c600e7c2e
Vladan Mitevski <vladan@mvdevelop.com>
2017-04-05 17:05:20 +0200

    Improve blog footer

--- blog/wp-content/themes/mvdevelop/footer.php
+++ blog/wp-content/themes/mvdevelop/footer.php
@@ -1,23 +1,52 @@
-  <footer class="footer">
+  <footer id="footer" class="footer">
     <div class="container-fluid">
+
       <div class="row">
-
-        <div class="col-sm-9">
-          <div class="row">
-            <div class="col-sm-4">
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 1') ) : ?><?php endif; ?>
-            </div>
-            <div class="col-sm-4">
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 2') ) : ?><?php endif; ?>
-            </div>
-            <div class="col-sm-4">
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 3') ) : ?><?php endif; ?>
-            </div>
-          </div>
-          <p class="copy">&copy; Superior Develop <?php echo date('Y'); ?>, All Rights Reserved</p>
+        <div class="col-sm-12">
+          <ul class="footer-social">
+            <li>
+              <a href="https://twitter.com/mvdevelop" target="_blank" title="Follow us on Twitter">
+                <i class="fa fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/vladanmitevski" target="_blank" title="Connect with us on LinkedIn">
+                <i class="fa fa-linkedin"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/mvdevelop/" target="_blank" title="Follow us on Facebook">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/mvdevelop/" target="_blank" title="Follow us on Facebook">
+                <i class="fa fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/mvdevelop" target="_blank" title="Follow us on Facebook">
+                <i class="fa fa-github"></i>
+              </a>
+            </li>
+          </ul>
         </div>
-
       </div>
+
+      <div class="row">
+        <div class="col-sm-12">
+          <?php wp_nav_menu( array( 'menu' => 'Main nav', 'menu_class' => 'footer-nav', 'theme_location' => 'primary' ) ); ?>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="copyright">
+            <p>Copyright @ 2012-<?php echo date('Y'); ?> MV Develop. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   </footer>
 
